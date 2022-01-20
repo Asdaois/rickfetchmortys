20a494621430582ede00796c2f3a9bf060482509
Jose Guevara <josexx1234@gmail.com>
2022-01-20 19:24:51 -0400

    test(withDuration): pass

---
+++ src/helpers/withDuration.js
@@ -0,0 +1,23 @@
+/**
+ *  Get a function that return a object and add the execution time 
+ *  @param {() => ({})} callback
+ */
+const withDuration = async (callback) => {
+  const startTime = Date.now();
+
+  const result = await callback();
+
+  const duration = Date.now() - startTime;
+
+  const seconds = Math.floor(duration / 1000);
+  const miliSeconds = duration - seconds * 1000;
+  const time = `${seconds}s ${miliSeconds}ms`;
+
+  if (process.env.NODE_ENV !== 'test') {
+    return { ...result, time };
+  }
+
+  return { duration: Math.floor(duration), time };
+};
+
+module.exports = withDuration;
---
+++ src/helpers/withDuration.test.js
@@ -0,0 +1,19 @@
+const withDuration = require('./withDuration');
+
+test('duration is greater than 3 seconds', async () => {
+  const callback = async () => {
+    await new Promise((r) => setTimeout(r, 3500));
+  };
+  const result = await withDuration(callback);
+
+  expect(result.duration).toBeGreaterThan(3500);
+});
+
+test('time to contain 3s', async () => {
+  const callback = async () => {
+    await new Promise((r) => setTimeout(r, 3510));
+  };
+  const result = await withDuration(callback);
+  expect(result.time).toMatch(/3s/);
+  expect(result.time).toMatch(/ms/);
+});
